ff417fbdf446dd91a9256cdc876e6c5b08c486f0
Dudi Lugasi <dulu123@gmail.com>
2015-04-30 14:11:29 +0300

    now with api

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.get('/api',function(req,res){
 //if the route is '/textbooks' the server returns json object with all the textbooks
 app.get('/textbooks',function(req,res) {
     console.log('getting all textbooks');
+    res.append("Access-Control-Allow-Origin", "*")
     res.json(textbooks.getAllTextBooks());
 });
 
